aa61117f5f5063e2f47088a9d8122ba592da1b17
锋 杨 <yangf85@163.com>
2025-06-26 20:49:25 +0800

    添加项目文件。

---
+++ BlazorLearning.Api/Program.cs
@@ -0,0 +1,31 @@
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference(options =>
+    {
+        options.WithTitle("我的 API 文档")
+               .WithTheme(ScalarTheme.BluePlanet) // 或其他主题
+               .WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.HttpClient);
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
